cadcd6be879f8b343a69f7c96db04b23db809c3f
Tim Fielder <fiel1139@gmail.com>
2018-12-05 11:23:11 -0700

    Add seed file.

--- seeds/dev/songs.js
+++ seeds/dev/songs.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+  return knex('songs').del()
+    .then(() => {
+      return Promise.all([
+        knex('songs').insert({name: "We Are the Champions", artist_name: "Queen", genre: "Rock", song_rating: 99}, 'id'),
+        knex('songs').insert({name: "We Will Rock You", artist_name: "Queen" , genre: "Rock", song_rating: 77}, 'id'),
+        knex('songs').insert({name: "Somebody to Love", artist_name: "Queen" , genre: "Rock", song_rating: 88}, 'id'),
+        knex('songs').insert({name: "I Want to Break Free", artist_name: "Queen" , genre: "Rock", song_rating: 99}, 'id')
+    .then(() => console.log('Seeding complete!'))
+    .catch(error => console.log(`Error seeding data: ${error}`))
+    ])
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+  };
