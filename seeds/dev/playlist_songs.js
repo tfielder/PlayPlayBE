71c858a20f8a79cb8a863b9e54d44888e4796d75
Tim Fielder <fiel1139@gmail.com>
2018-12-06 13:58:26 -0700

    Reinstate playlist songs.js file.

--- seeds/dev/playlist_songs.js
+++ seeds/dev/playlist_songs.js
@@ -1,16 +1,17 @@
-// exports.seed = function(knex, Promise) {
-//   return knex('playlist_songs').del()
-//     .then(() => {
-//       return Promise.all([
-//         // knex('playlist_songs').insert({song_id: 1, playlist_id: 1}, 'id'),
-//         // knex('playlist_songs').insert({song_id: 2, playlist_id: 1}, 'id'),
-//         // knex('playlist_songs').insert({song_id: 1, playlist_id: 2}, 'id'),
-//         // knex('playlist_songs').insert({song_id: 2, playlist_id: 2}, 'id')
-//       .then(() => console.log('Seeding Playlist Songs Complete!'))
-//       .catch(error => console.log(`Error seeding data: ${error}`))
-//       ])
-//     })
-//     .catch(error => console.log(`Error seeding data: ${error}`))
-//   };
-//
-//
+exports.seed = function(knex, Promise) {
+  return knex('playlist_songs').del()
+    .then(() => {
+      return Promise.all([
+        knex('playlist_songs').insert('id')
+        // knex('playlist_songs').insert({song_id: 1, playlist_id: 1}, 'id'),
+        // knex('playlist_songs').insert({song_id: 2, playlist_id: 1}, 'id'),
+        // knex('playlist_songs').insert({song_id: 1, playlist_id: 2}, 'id'),
+        // knex('playlist_songs').insert({song_id: 2, playlist_id: 2}, 'id')
+      .then(() => console.log('Seeding Playlist Songs Complete!'))
+      .catch(error => console.log(`Error seeding data: ${error}`))
+      ])
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`))
+  };
+
+
