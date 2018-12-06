4588b3d8b55c5520ec78f3adcdbe3ca2ef5a9216
Tim Fielder <fiel1139@gmail.com>
2018-12-06 14:12:16 -0700

    Remove comments in playlist_songs.js in seeds.

--- seeds/dev/playlist_songs.js
+++ seeds/dev/playlist_songs.js
@@ -3,10 +3,6 @@ exports.seed = function(knex, Promise) {
     .then(() => {
       return Promise.all([
         knex('playlist_songs').insert('id')
-        // knex('playlist_songs').insert({song_id: 1, playlist_id: 1}, 'id'),
-        // knex('playlist_songs').insert({song_id: 2, playlist_id: 1}, 'id'),
-        // knex('playlist_songs').insert({song_id: 1, playlist_id: 2}, 'id'),
-        // knex('playlist_songs').insert({song_id: 2, playlist_id: 2}, 'id')
       .then(() => console.log('Seeding Playlist Songs Complete!'))
       .catch(error => console.log(`Error seeding data: ${error}`))
       ])
