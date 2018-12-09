5c0a83cf63064c4de4298e306628892e9cd25e28
Tim Fielder <fiel1139@gmail.com>
2018-12-09 14:32:57 -0700

    Export app from server.js file.

--- server.js
+++ server.js
@@ -241,3 +241,7 @@ app.delete('/api/v1/songs/:id', (request, response) => {
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+module.exports = {
+  app,
+}
