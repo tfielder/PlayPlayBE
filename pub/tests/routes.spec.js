70c310d5248d24147f42097747be698b6db82a72
Tim Fielder <fiel1139@gmail.com>
2018-12-09 14:40:47 -0700

    Create test for route root.

--- pub/tests/routes.spec.js
+++ pub/tests/routes.spec.js
@@ -6,7 +6,16 @@ const server = require('../../server');
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-
+  it('should return the homepage with text', done => {
+    chai.request(server)
+    .get('/')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.html;
+      response.res.text.should.equal('Welcome!');
+      done();
+    });
+  });
 });
 
 describe('API Routes', () => {
