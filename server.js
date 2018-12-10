81ff206eeb48c51c1669ee78f359b04e541e866f
Tim Fielder <fiel1139@gmail.com>
2018-12-10 12:59:36 -0700

    Reduce server.js to use songs.

--- server.js
+++ server.js
@@ -145,97 +145,10 @@ app.delete('/api/v1/playlists/:playlist_id/songs/:id', (request, response) => {
 });
 
 // Songs Section
-//Create
-app.post('/api/v1/songs', (request, response) => {
-  const song = request.body;
-  for (let requiredParameter of ['name', 'artist_name', 'genre', 'song_rating']){
-    if (!song[requiredParameter]){
-      return response.status(400).send({ error : `Expected format: { name: <String>, artist_name: <String>, genre: <String>, song_rating: <Integer>}. You're missing a "${requiredParameter}" property.`});
-    }
-  }
-
-  Song.create_song(song)
-  //database('songs').insert(song, 'id').returning('*')
-    .then(song => {
-      const songs = song[0]
-      response.status(201).json({ songs })
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-});
-
-//Read
+app.use('/api/v1/songs', songs_routes);
 app.use('/api/v1/favorites', songs_routes);
-// app.get('/api/v1/favorites', (request, response) => {
-//
-// });
 
-app.get('/api/v1/songs/:id', (request, response) => {
-
-  Song.find_by_id(request.params.id)
-    .then(song => {
-      if (song.length) {
-        response.status(200).json(song);
-      } else {
-        response.status(404).json({
-          error: `Could not find song with id ${request.params.id}`
-        });
-      }
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-});
-
-//Update
-app.put('/api/v1/songs/:id', (request, response) => {
-  const song = request.body;
-  const id = parseInt(request.params.id);
-
-  for (let requiredParameter of ['id', 'name', 'artist_name', 'genre', 'song_rating']){
-    if (!song["songs"][requiredParameter]){
-      return response
-        .status(422)
-        .send({ error: `Expected format: { name: <String>, artist_name: <String>, genre: <String>, song_rating: <Integer>}. You're missing a "${requiredParameter}" property.`});
-    }
-  }
-
-  let update_info = {
-    name: request.body["songs"]['name'],
-    artist_name: request.body["songs"]['artist_name'],
-    genre: request.body["songs"]['genre'],
-    song_rating: request.body["songs"]['song_rating']
-  };
-
-  //Song.update_song(id, update_info)
-  Song.update_song(request.params.id, update_info)
-  //database('songs').where('id', request.params.id).update(update_info).returning('*')
-    .then(song => {
-      response.status(200).json({ song })
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-});
-
-//Destroy
-app.delete('/api/v1/songs/:id', (request, response) => {
-  //const song = database('songs').where('id', request.params.id).select();
-  const song = Song.find_by_id(request.params.id);
-  if (song) {
-    Song.delete_song(request.params.id)
-    //database('songs').where('id', request.params.id).del().returning('*')
-      .then(song => {
-        return response.status(204);
-      })
-      .catch(error => {
-        return response.status(500).json({ error });
-      })
-  } else {
-    return response.status(404).send({ error: `Could not find song with id ${request.params.id}` });
-  }
- });
+//***stopping here
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
